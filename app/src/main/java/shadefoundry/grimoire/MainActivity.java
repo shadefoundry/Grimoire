faf86b211fafca102d51e4f58004d18ed7ba96c3
Kevin Lopez <link4912@gmail.com>
2018-11-13 21:16:16 -0500

    Small update
    Basically just finishing off erasing the navigation drawer from existence since we aren't using it anymore and frankly moving everything to a new project at this stage would be the definition of insanity.

--- app/src/main/java/shadefoundry/grimoire/MainActivity.java
+++ app/src/main/java/shadefoundry/grimoire/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
